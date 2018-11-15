0dcbfd87e611944d8238e5747ac8c28d0bdab5c8
Chongke Wu <chongkewu@email.arizona.edu>
2018-11-15 15:18:08 -0700

    revise the main_sh

--- main.sh
+++ main.sh
@@ -2,21 +2,26 @@
 cd /home/u32/chongkewu/PESC/Spearmint-PESC/spearmint
 exp_folder="simple"
 Num=5
-##cleanup
-python /home/u32/chongkewu/PESC/Spearmint-PESC/spearmint/cleanup.py /home/u32/chongkewu/PESC/Spearmint-PESC/examples/$exp_folder
-##run_exp
+Time=20
+###cleanup multiple experiments
+#python /home/u32/chongkewu/PESC/Spearmint-PESC/spearmint/cleanup_experiments.py /home/u32/chongkewu/PESC/Spearmint-PESC/examples/$exp_folder $Num 
+#sleep 3s
+###run_exp
 #python /home/u32/chongkewu/PESC/Spearmint-PESC/spearmint/main.py /home/u32/chongkewu/PESC/Spearmint-PESC/examples/$exp_folder
-##look_result
+###look_result
 #python /home/u32/chongkewu/PESC/Spearmint-PESC/spearmint/print_all_results.py /home/u32/chongkewu/PESC/Spearmint-PESC/examples/$exp_folder
 
 ################################################################################
 
-##repeat_exp
+###repeat_exp
 python ./run_experiments.py /home/u32/chongkewu/PESC/Spearmint-PESC/examples/$exp_folder $Num
-
-##look_curve
+echo "wait $Time seconds"
+sleep $Time
+###look_curve
+echo "Check the current result, may be program still running"
 python /home/u32/chongkewu/PESC/Spearmint-PESC/spearmint/visualizations/progress_curve.py /home/u32/chongkewu/PESC/Spearmint-PESC/examples/$exp_folder --repeat=$Num
-##open_curve
+sleep 3s
+###open_curve
 xdg-open /home/u32/chongkewu/PESC/Spearmint-PESC/examples/$exp_folder/plots/mean_error.pdf
 
 
